3b3bbc6357a800f484ac073eec22533512de17e1
petrolingus <petrolingus@gmail.com>
2022-04-21 04:34:32 +0300

    Service3d renamed to CanvasUpdateService

--- src/main/java/me/petrolingus/modsys/twosourceinterference/CanvasUpdateService.java
+++ src/main/java/me/petrolingus/modsys/twosourceinterference/CanvasUpdateService.java
@@ -8,52 +8,43 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.PixelFormat;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
-import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
 
 import java.nio.ByteBuffer;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
-import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
-
-public class Service3d extends Service<Void> {
+public class CanvasUpdateService extends Service<Void> {
 
     private final Canvas canvas;
 
-    public Service3d(Canvas canvas) {
+    public CanvasUpdateService(Canvas canvas) {
         this.canvas = canvas;
     }
 
     @Override
     protected Task<Void> createTask() {
-        return new Task<Void>() {
+        return new Task<>() {
             @Override
-            protected Void call() throws Exception {
+            protected Void call() {
 
-                int width = 800;
-                int height = 800;
+                int width = (int) canvas.getWidth();
+                int height = (int) canvas.getHeight();;
                 int bpp = 4; // Assuming a 32-bit display with a byte each for red, green, blue, and alpha.
-                int whb = width * height * bpp;
-                int[] pixels = new int[width * height];
+
+                // Update canvas every 16 ms
                 final WritableImage img = new WritableImage(width, height);
-                PixelWriter pw = img.getPixelWriter();
-
                 ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-                executor.scheduleAtFixedRate(() -> {
-                    Platform.runLater(() -> {
-                        GraphicsContext g = canvas.getGraphicsContext2D();
-//                        g.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-                        g.drawImage(img, 0, 0);
-                    });
-                }, 0, 32, TimeUnit.MILLISECONDS);
+                executor.scheduleAtFixedRate(() -> Platform.runLater(() -> {
+                    GraphicsContext g = canvas.getGraphicsContext2D();
+                    g.drawImage(img, 0, 0);
+                }), 0, 16, TimeUnit.MILLISECONDS);
 
+                // Prepare canvas data
+                int[] pixels = new int[width * height];
+                PixelWriter pw = img.getPixelWriter();
                 while (!isCancelled()) {
-
                     ByteBuffer buffer = LwjglApplication.buffer;
-
                     for (int y = 0; y < width; y++) {
                         for (int x = 0; x < height; x++) {
                             int i = (x + (width * y)) * bpp;
@@ -61,14 +52,12 @@ public class Service3d extends Service<Void> {
                             int g = buffer.get(i + 1) & 0xFF;
                             int b = buffer.get(i + 2) & 0xFF;
                             pixels[(height - (y + 1)) * 800 + x] = (0xFF << 24) | (r << 16) | (g << 8) | b;
-//                            pixels[800 * x + y] = ThreadLocalRandom.current().nextInt();
                         }
                     }
-
                     pw.setPixels(0, 0, width, height, PixelFormat.getIntArgbInstance(), pixels, 0, width);
-
                 }
 
+                // When service done we need to shut down the scheduler executor
                 executor.shutdown();
 
                 return null;
