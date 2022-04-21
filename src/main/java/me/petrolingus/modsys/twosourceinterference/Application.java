dbae01c9643e54e9f54d802d8e04285c74cd13e4
petrolingus <petrolingus@gmail.com>
2022-04-21 04:57:29 +0300

    Refactor Application class

--- src/main/java/me/petrolingus/modsys/twosourceinterference/Application.java
+++ src/main/java/me/petrolingus/modsys/twosourceinterference/Application.java
@@ -1,14 +1,8 @@
 package me.petrolingus.modsys.twosourceinterference;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-
-import java.io.IOException;
 
 public class Application extends javafx.application.Application {
     @Override
@@ -18,19 +12,12 @@ public class Application extends javafx.application.Application {
         stage.setTitle("TSI");
         stage.setScene(scene);
         stage.show();
-        stage.onCloseRequestProperty().addListener((observable, oldValue, newValue) -> {
+        stage.showingProperty().addListener((observable, oldValue, newValue) -> {
             System.exit(0);
         });
     }
 
-    public static void main(String[] args) throws Exception {
-//        new Thread(() -> {
-//            try {
-//                new LwjglApplication().run();
-//            } catch (Exception e) {
-//                throw new RuntimeException(e);
-//            }
-//        }).start();
+    public static void main(String[] args) {
         launch();
     }
 }
